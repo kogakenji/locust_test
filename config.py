af71902526d0edd34d582f7c59594b2b83e44329
kogakenji <koga.k3nj1@gmail.com>
2019-07-28 23:23:07 +1000

    User profile and error handler
    Defined how to handle avatar using gravatar
    Error handling using logging to a file and email
    Profile handling

--- config.py
+++ config.py
@@ -10,3 +10,11 @@ class Config(object):
         'sqlite:///' + os.path.join(basedir, 'app.db')
     # not to signal for every app change in the database
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Configure e-mail server to handle errors
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['koga.k3nj1@gmail.com']
--- locustfile.py
+++ locustfile.py
@@ -4,7 +4,7 @@ from locust import HttpLocust, TaskSet, task
 class UserBehavior(TaskSet):
 
     def on_start(self):
-        """ on_start is called when a Locust start before any task is scheduled"""
+        """ on_start is called when a Locust start before any task is scheduled""" # noqa E501
         self.login()
 
     def on_stop(self):
@@ -12,7 +12,7 @@ class UserBehavior(TaskSet):
         self.logout()
 
     def login(self):
-        self.client.post("/login", {"username": "kenji", "password": "password"})
+        self.client.post("/login", {"username": "kenji", "password": "password"}) # noqa E501
 
     def logout(self):
         self.client.post("/logout")
